ce343bb18046dacd4f31ccc94a1d69ab26d690cb
nirglus <nirgluskin@gmail.com>
2024-01-28 20:03:23 +0200

    add user context

---
+++ src/context/User.jsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useEffect } from "react";
+import { auth } from "../config/firebaseConfig";
+
+export const UserContext = createContext({});
+
+export default function UserProvider({children}){
+    const [user, setUser] = useState(null);
+    useEffect(() =>{ 
+        onAuthStateChanged(auth, (userCardential) =>{
+            if (userCardential){
+                setUser({ email: userCardential.email, id: userCardential.uid});
+                console.log(user);
+            }
+        })}, [])
+    
+    const userAuth = getAuth();
+    
+    const userSignOut = () => {
+        signOut(userAuth)
+            .then(() => {
+              console.log("Signed out");
+            })
+            .catch((error) => {
+              console.error("Error signing out: ", error);
+            })
+            .finally(() => {
+              setUser(null); 
+            });
+        };
+    return(
+       <UserContext.Provider value={{user, setUser, userSignOut}}>
+        {children}
+       </UserContext.Provider>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import UserProvider from './context/User.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <UserProvider>
     <App />
-  </React.StrictMode>,
+  </UserProvider>
+  // </React.StrictMode>,
 )
