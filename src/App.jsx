6bd2b0c7e8b7b65c707cba2763987b72c16d2064
nirglus <nirgluskin@gmail.com>
2024-01-31 14:39:18 +0200

    complete home page

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
     <>
     <BrowserRouter>
       <Navbar />
-      <h1>Hello</h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/resume" element={<Resume />} />
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,21 @@
-
+import "./Home.css";
+import { UserContext } from "../../context/User";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 function Home() {
+  const {user} = useContext(UserContext);
   return (
-    <div>
-      
-    </div>
+    <div className="homeTop">
+        <h1>Crafting the <span className="matrix">perfect</span> resume is now <span className="matrix">easier</span> than ever!</h1>
+        <ul>
+          <li>Impressive resumes, Effortless rafting</li>
+          <li>Real-time preview & Pre-written examples</li>
+          <li>Dozens of HR approved templates</li>
+          <li>Create your winning resume in just 15 minutes</li>
+        </ul>
+        <button>{user? (
+        <Link to="/resume">Create a resume!</Link>) : (<Link to="/login">Try it out!</Link>)}</button>
+      </div>
   )
 }
 
