96c5fbf8eef073e618426a7d33dfe48c0b2ca2af
nirglus <nirgluskin@gmail.com>
2024-02-01 12:01:42 +0200

    adjust resume page

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -5,7 +5,7 @@ import logo from "../../assets/logo.png";
 import "./Navbar.css";
 
 function Navbar() {
-  const {user, userSignOut, currentUser} = useContext(UserContext);
+  const {userSignOut, currentUser} = useContext(UserContext);
   return (
     <nav>
         <div className="logo">
@@ -13,11 +13,11 @@ function Navbar() {
             <h1>Resume<span className="matrix">Matrix</span></h1>
         </div>
         <div className="navItems">
-            <Link to="/" style={{padding: "8px"}}>Home</Link>
-            <Link to="/resume" style={{padding: "8px"}}>Resume</Link>            
+            <Link to="/">Home</Link>
+            <Link to="/resume">Resume</Link>            
             {currentUser ? 
             (<button onClick={userSignOut}>Sign out,<span> <b>{currentUser.nickname}</b></span></button>)
-             : (<Link to ="/login" style={{padding: "8px"}}>Login</Link>)
+             : (<Link to ="/login">Login</Link>)
              }
         </div>
     </nav>
--- src/pages/Resume/index.jsx
+++ src/pages/Resume/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Builder from '../../components/Builder'
 import Display from '../../components/Display'
+import { UserContext } from '../../context/User'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import "./Resume.css";
 
 function Resume() {
+  const {user} = useContext(UserContext);
   return (
     <div>
-      <Builder/>
-      <Display />
+      {user ? (
+        <>
+          <Builder/>
+          <Display />
+        </>
+      ): (
+        <div className='noUser'>
+        <h1>Please <Link className="matrix" to="/login">login</Link> or <Link className="matrix" to="/login">register</Link></h1>
+        </div>
+      )}
     </div>
   )
 }
