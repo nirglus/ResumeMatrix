c33ef2f5df741df1a26c1934c8bf6030a146e06e
nirglus <nirgluskin@gmail.com>
2024-03-03 10:28:22 +0200

    adjust fonts

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -29,10 +29,7 @@ function Navbar() {
             </div>
             <ul>
               <li><Link to="/" onClick={closeMobileMenu}><i class="bi bi-house"></i> Home</Link></li>
-              <li><Link to="/resume" onClick={closeMobileMenu}><i class="bi bi-file-earmark-person"></i> Resume</Link>  </li>
-              <li></li>
-              <li></li>
-                        
+              <li><Link to="/resume" onClick={closeMobileMenu}><i class="bi bi-file-earmark-person"></i> Resume</Link>  </li>                  
               {currentUser ? 
               (<li><button onClick={userSignOut}><i class="bi bi-box-arrow-right"></i> Sign out,<span> <b>{currentUser.nickname}</b></span></button></li>)
               : (<li><Link to ="/login"><i class="bi bi-box-arrow-in-right"></i> Login</Link></li>)
