1951ee50263ade486eeee6ce94f301ca786f4553
nirglus <nirgluskin@gmail.com>
2024-03-03 14:25:35 +0200

    fix navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import { UserContext } from "../../context/User";
-import { Link } from "react-router-dom";
+import { NavLink} from "react-router-dom";
 import logo from "../../assets/logo.png";
 import "./Navbar.css";
 
@@ -12,56 +12,63 @@ function Navbar() {
     setIsOpen(!isOpen);
     };
 
-
+  const closeMenu = () => {
+      setIsOpen(false); 
+    };
+  
   return (
-    <nav>
-      <div className="logo">
-        <img className="logoImage" src={logo} alt="ResumeMatrix-Logo" />
-        <h1>
-          Resume<span className="matrix">Matrix</span>
-        </h1>
-      </div>
-      <div className={`myNavItems ${isOpen ? "active" : ""}`}>
-        <div className="hamburger" onClick={toggleMenu}>
-          <div className="line"></div>
-          <div className="line"></div>
-          <div className="line"></div>
+      <nav>
+        <div className="logo">
+          <img className="logoImage" src={logo} alt="ResumeMatrix-Logo" />
+          <h1>
+            Resume<span className="matrix">Matrix</span>
+          </h1>
         </div>
-        <ul>
-          <li>
-            <Link to="/">
-              <i class="bi bi-house"></i> Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/resume">
-              <i class="bi bi-file-earmark-person"></i> Resume
-            </Link>
-          </li>
-          {currentUser ? (
-            <>
-            <li><Link to={`/account/${currentUser.id}`}><i class="bi bi-list-stars"></i> My Resumes</Link></li>
+        <div className={`myNavItems ${isOpen ? "active" : ""}`}>
+          <div className="hamburger" onClick={toggleMenu}>
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>
+          </div>
+          <ul>
             <li>
-              <button onClick={userSignOut}>
-                <i class="bi bi-box-arrow-right"></i> Sign out,
-                <span>
-                  {" "}
-                  <b>{currentUser.nickname}</b>
-                </span>
-              </button>
+              <NavLink exact to="/" onClick={closeMenu}> 
+                <i className="bi bi-house"></i> Home
+              </NavLink>
             </li>
-            </>
-          ) : (
             <li>
-              <Link to="/login">
-                <i class="bi bi-box-arrow-in-right"></i> Login
-              </Link>
+              <NavLink to="/resume" onClick={closeMenu}>
+                <i className="bi bi-file-earmark-person"></i> Resume
+              </NavLink>
             </li>
-          )}
-        </ul>
-      </div>
-    </nav>
-  );
+            {currentUser ? (
+              <>
+                <li>
+                  <NavLink to={`/account/${currentUser.id}`} onClick={closeMenu}>
+                    <i className="bi bi-list-stars"></i> My Resumes
+                  </NavLink>
+                </li>
+                <li>
+                  <button onClick={userSignOut}>
+                    <i className="bi bi-box-arrow-right"></i> Sign out,{" "}
+                    <span>
+                      {" "}
+                      <b>{currentUser.nickname}</b>
+                    </span>
+                  </button>
+                </li>
+              </>
+            ) : (
+              <li>
+                <NavLink to="/login" onClick={closeMenu}>
+                  <i className="bi bi-box-arrow-in-right"></i> Login
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </div>
+      </nav>
+    );
 }
 
 export default Navbar;
